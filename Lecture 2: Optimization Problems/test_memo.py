f735543d097852c70508bab48c64e97ee7ffec4e
jalil_kartal <Jalil.Nourmohammadi@elka.pw.edu.pl>
2024-05-25 22:37:00 +0200

    work on knapsack using memoization

--- Lecture 2: Optimization Problems/test_memo.py
+++ Lecture 2: Optimization Problems/test_memo.py
@@ -2,9 +2,9 @@ from memoization_knapsack import knapsack
 
 if __name__ == "__main__":
     # Example usage
-    max_cost = 150  # Maximum capacity of knapsack
+    cost = 150  # Maximum capacity of knapsack
     weights = [10, 20, 30]  # Weights of items
     values = [60, 100, 120]  # Values of items
 
-    max_value = knapsack(max_cost, weights, values)
+    max_value = knapsack(cost, weights, values)
     print("Maximum value in Knapsack:", max_value)
