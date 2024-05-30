8039724a3f3dbb34ca741dce64a9a5453e034ef0
jalil_kartal <Jalil.Nourmohammadi@elka.pw.edu.pl>
2024-05-30 22:38:16 +0200

    work on traversing

--- utils/dfs.py
+++ utils/dfs.py
@@ -16,3 +16,19 @@ def dfs(graph, start, end, path, shortest, to_print=False):
         elif to_print:
             print("Already existed", node)
     return shortest
+
+def short_path(graph, start, end, to_print=False):
+    return dfs(graph, start, end, {}, None, to_print)
+
+
+def print_path(path):
+    """
+    Prints the path in a readable format.
+
+    Args:
+    path (list): A list of nodes representing the path.
+
+    Returns:
+    str: A string representation of the path.
+    """
+    return ' -> '.join([str(node) for node in path])
