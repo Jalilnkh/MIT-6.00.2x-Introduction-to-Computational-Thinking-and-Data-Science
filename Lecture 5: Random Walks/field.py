8dbfa85e22add82aa5788f4fc5a05cce408445f1
jalil_kartal <Jalil.Nourmohammadi@elka.pw.edu.pl>
2024-06-16 23:38:01 +0200

    work on random walk

--- Lecture 5: Random Walks/field.py
+++ Lecture 5: Random Walks/field.py
@@ -1,5 +1,5 @@
-class Field():
-    def __init__(self) -> None:
+class Field:
+    def __init__(self):
         self.drunks = {}
 
     def add_drunk(self, drunk, loc):
@@ -7,15 +7,16 @@ class Field():
             raise ValueError('Duplicate drunk')
         else:
             self.drunks[drunk] = loc
-    def get_loc(self, drunk):
-        if drunk not in self.drunks:
-            raise ValueError('Drunk not in field')
-        return self.drunks[drunk]
-    
+
     def move_drunk(self, drunk):
         if drunk not in self.drunks:
             raise ValueError('Drunk not in field')
         x_dist, y_dist = drunk.take_step()
+        current_location = self.drunks[drunk]
+        # Move to new location
+        self.drunks[drunk] = current_location.move(x_dist, y_dist)
 
-        self.drunks[drunk].move(x_dist, y_dist)
-   
+    def get_loc(self, drunk):
+        if drunk not in self.drunks:
+            raise ValueError('Drunk not in field')
+        return self.drunks[drunk]
