1079af28559c8ad9150a73ca2f09e8704e6f2eb7
jalil_kartal <Jalil.Nourmohammadi@elka.pw.edu.pl>
2024-07-16 20:41:00 +0200

    finish lecture 9

--- Lecture 9: Understanding Experimental Data/lecture_code.py
+++ Lecture 9: Understanding Experimental Data/lecture_code.py
@@ -1,4 +1,5 @@
 import pylab
+import numpy as np
 
 def get_data():
     pass
@@ -35,4 +36,31 @@ def fit_data(dist, mass, degree=1, file_name=None, model_label='Linear Model'):
         'r', 
         label=model_label)
     pylab.legend(loc= 'best')
+
+
+def gen_fits(x_vals, y_vals, degrees):
+    models = []
+    for d in degrees:
+        model = pylab.polyfit(x_vals, y_vals, d)
+        models.append(model)
+    return models
+
+
+def test_fits(models, degrees, x_vals, y_vals, title):
+    pylab.plot(x_vals, y_vals, 'o', label='Data')
+    for i in range(len(models)):
+        est_y_vals = pylab.polyval(models[i], x_vals)
+        error = r_squared(y_vals, est_y_vals)
+        pylab.plot(x_vals, est_y_vals,
+            label=f'Fit of degree {str(degrees[i])}, R2={str(round(error, 5))}')
+    pylab.legend(loc='best')
+    pylab.title(title)
+
+
+def r_squared(observed, predicted):
+    error = ((predicted - observed)**2).sum()
+    mean_error = error/len(observed)
+    return 1 - (mean_error/np.var(observed))
+
+
     
